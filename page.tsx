eb214324e68d290471d0b5c411d2f3ee72db17b8
Farhat Naz <159726939+Farhat-Naz@users.noreply.github.com>
2024-11-08 02:49:18 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Prop from '../../components/prop'
+
+function Page() {
+  return (
+    <div>
+      <h3>props</h3>
+      <Prop name="farhat" id= "38"  Lname="Naz"/>
+    </div>
+  )
+}
+
+export default Page
